23c5766024b4e6c530936634182c3a89b39772e7
16369 <ruanweier@gzmpc.com>
2020-03-15 11:32:38 +0800

    0.2.9.1

---
+++ exframework-support-rest/src/main/java/com/gzmpc/support/rest/filter/CorsResponseFilter.java
@@ -0,0 +1,41 @@
+package com.gzmpc.support.rest.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.entity.StringEntity;
+
+/**
+* @author rwe
+* @version 创建时间：Feb 14, 2020 1:27:12 PM
+* 跨域解决办法
+*/
+
+public class CorsResponseFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+		//跨域请求，*代表允许全部类型
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+		//允许请求方式
+		responseContext.getHeaders().add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		//用来指定本次预检请求的有效期，单位为秒，在此期间不用发出另一条预检请求
+		responseContext.getHeaders().add("Access-Control-Max-Age", "3600");
+		//请求包含的字段内容，如有多个可用哪个逗号分隔如下
+		responseContext.getHeaders().add("Access-Control-Allow-Headers", "content-type,x-requested-with,Authorization, x-ui-request,lang");
+		//访问控制允许凭据，true为允许
+		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		// 浏览器是会先发一次options请求，如果请求通过，则继续发送正式的post请求
+        // 配置options的请求返回
+		if ("OPTIONS".equals(requestContext.getMethod())) {
+			responseContext.setStatus(HttpStatus.SC_OK);
+			responseContext.setEntity(new StringEntity("OPTIONS returns OK","utf-8"));
+		}
+	}
+
+}
