c050438c3f258ce2cf24a00f2962e9a6be5235ab
19394 <yejianfeng@gzmpc.com>
2020-06-21 11:17:09 +0800

    增加微服务单点登录

---
+++ exframework-support-tsf/exframework-tsf-support-sso/src/main/java/com/gzmpc/support/sso/core/constant/UserConstants.java
@@ -0,0 +1,27 @@
+package com.gzmpc.support.sso.core.constant;
+
+public interface UserConstants {
+
+
+    //用户类型
+    String USER_TYPE_ADMIN="ADMIN";
+
+    String USER_TYPE_SUPERADMIN="SUPERADMIN";
+
+    String USER_TYPE_EMPLOYEE="EMPLOYEE";
+
+    String USER_TYPE_SUPPLYER="SUPPLYER";
+
+    String USER_TYPE_CUSTOMER="CUSTOMER";
+
+    //帐号来源 内部OA
+    String USER_ACCOUNT_SOURCE_OA="OA";
+
+    //帐号来源 流向平台
+    String USER_ACCOUNT_SOURCE_FLOW="FLOW";
+
+    //帐号关联系统
+    String USER_ACCOUNT_APPSOURCE_SIP="SIP";
+
+
+}
