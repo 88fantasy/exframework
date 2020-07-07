8a585ab1bfa0c9db8e71d33c5d2e4bde854c8772
88fantasy <88fantasy@gmail.com>
2020-07-07 11:52:23 +0800

    修正 LoginCurrenrtUser  -> LoginCurrentUser

--- exframework-support-tsf/exframework-tsf-support-sso/src/main/java/com/gzmpc/support/sso/core/annotation/LoginCurrentUser.java
+++ exframework-support-tsf/exframework-tsf-support-sso/src/main/java/com/gzmpc/support/sso/core/annotation/LoginCurrentUser.java
@@ -13,6 +13,6 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface LoginCurrenrtUser {
+public @interface LoginCurrentUser {
 
 }
