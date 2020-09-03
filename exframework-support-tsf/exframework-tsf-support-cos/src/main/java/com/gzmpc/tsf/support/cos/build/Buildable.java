8d58bc4d2280b703d508fe579a74c6fd726d4724
88fantasy <88fantasy@gmail.com>
2020-09-03 09:38:47 +0800

    创建cos(文件上传)组件

---
+++ exframework-support-tsf/exframework-tsf-support-cos/src/main/java/com/gzmpc/tsf/support/cos/build/Buildable.java
@@ -0,0 +1,11 @@
+package com.gzmpc.tsf.support.cos.build;
+
+import com.gzmpc.tsf.support.cos.exception.BuildException;
+
+public interface Buildable {
+	
+	/*
+	 * 在环境启动时或重新读取配置时调用
+	 */
+	public void build() throws BuildException;
+}
