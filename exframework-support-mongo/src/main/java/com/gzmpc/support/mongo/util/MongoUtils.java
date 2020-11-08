d6e32c6c566a3549c90fb29e29e595a921dd2de4
88fantasy <88fantasy@gmail.com>
2020-11-08 19:26:13 +0800

    mongo support add page

---
+++ exframework-support-mongo/src/main/java/com/gzmpc/support/mongo/util/MongoUtils.java
@@ -0,0 +1,22 @@
+package com.gzmpc.support.mongo.util;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.gzmpc.support.common.entity.Page;
+import com.gzmpc.support.common.entity.PageModel;
+import com.gzmpc.support.common.entity.Pager;
+
+public class MongoUtils {
+
+	public <T> PageModel<T> queryListByPage(MongoTemplate template, Query query, Page page, Class<T> entityClass) {
+		long total = template.count(query, entityClass);
+		Pager pager = new Pager(total, page);
+		query.skip(page.skip()).limit(page.getPageSize());
+		List<T> list = template.find(query, entityClass);
+		PageModel<T> model = new PageModel<T>(pager, list);
+		return model;
+	}
+}
