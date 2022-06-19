4848f4fc3ce140afaaa5d54bddb6a3e116b12e50
ruanwe <88fantasy@gmail.com>
2022-06-20 00:37:21 +0800

    pom fix

--- exframework-support-doc/src/main/java/org/exframework/support/doc/controller/DocController.java
+++ exframework-support-doc/src/main/java/org/exframework/support/doc/controller/DocController.java
@@ -1,23 +1,19 @@
 package org.exframework.support.doc.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.annotations.ApiOperation;
 import org.exframework.support.doc.service.DataBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600, methods = {RequestMethod.GET})
-@Tag(name = "文档")
 public class DocController {
 
     @Autowired
     private DataBaseService dataBaseService;
 
-    @Operation(summary = "获取数据库物理表(markdown)")
+    @ApiOperation("获取数据库物理表(markdown)")
     @RequestMapping(value = "/database/md", method = RequestMethod.GET)
     public String markdown() {
         return dataBaseService.markdown();
--- exframework-support-jdbc/src/main/java/org/exframework/support/monitor/druid/filter/DruidStatFilter.java
+++ exframework-support-jdbc/src/main/java/org/exframework/support/monitor/druid/filter/DruidStatFilter.java
@@ -1,24 +1,24 @@
-package org.exframework.support.monitor.druid.filter;
-
-import com.alibaba.druid.support.http.WebStatFilter;
-
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-
-/**
- * @author rwe
- * @version 创建时间：2018年5月9日 下午3:22:56
- */
-
-@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = {
-		/** 忽略资源 */
-		@WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*,/static/*"),
-		/** 监控当前COOKIE的用户 */
-		@WebInitParam(name = "principalCookieName", value = "USER_COOKIE"),
-		/** 监控当前SESSION的用户 */
-		@WebInitParam(name = "principalSessionName", value = "USER_SESSION"),
-		/** 监控单个url访问数据库情况 */
-		@WebInitParam(name = "profileEnable", value = "true") })
-public class DruidStatFilter extends WebStatFilter {
-
-}
+//package org.exframework.support.monitor.druid.filter;
+//
+//import com.alibaba.druid.support.http.WebStatFilter;
+//
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.annotation.WebInitParam;
+//
+///**
+// * @author rwe
+// * @version 创建时间：2018年5月9日 下午3:22:56
+// */
+//
+//@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = {
+//		/** 忽略资源 */
+//		@WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*,/static/*"),
+//		/** 监控当前COOKIE的用户 */
+//		@WebInitParam(name = "principalCookieName", value = "USER_COOKIE"),
+//		/** 监控当前SESSION的用户 */
+//		@WebInitParam(name = "principalSessionName", value = "USER_SESSION"),
+//		/** 监控单个url访问数据库情况 */
+//		@WebInitParam(name = "profileEnable", value = "true") })
+//public class DruidStatFilter extends WebStatFilter {
+//
+//}
